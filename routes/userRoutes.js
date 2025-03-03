0c690d4c3e8dd9538b6bc7f283975a16db397d43
peagape <flaviorocha.dev@gmail.com>
2025-03-03 16:22:58 -0300

    ajuste delet user

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -5,9 +5,9 @@ const userController = require('../controllers/userController');
 
 router.post('/', userController.createUser);
 router.get('/', userController.getUsers);
-router.get('/:macid/info', userController.getUserInfo); // Agora busca por macid
-router.get('/:macid', userController.getUserById); // Caso deseje também buscar por macid
-router.put('/:macid', userController.updateUser);
-router.delete('/:macid', userController.deleteUser);// Agora busca por macid
+router.get('/:uid/info', userController.getUserInfo); // Agora busca por macid
+router.get('/:uid', userController.getUserById); // Caso deseje também buscar por macid
+router.put('/:uid', userController.updateUser);
+router.delete('/:uid', userController.deleteUser);// Agora busca por macid
 
 module.exports = router;
