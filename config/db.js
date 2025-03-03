0d667a9418c7a27a50dc464c70510c695c7d12ea
peagape <flaviorocha.dev@gmail.com>
2025-03-03 17:36:50 -0300

    ajuste db.js

--- config/db.js
+++ config/db.js
@@ -9,41 +9,41 @@ const db = new sqlite3.Database('./database.db', (err) => {
   }
 });
 
-// Criação das tabelas
-// db.serialize(() => {
-//   // Tabela de usuários
-//   db.run(`CREATE TABLE IF NOT EXISTS usuarios (
-//     uid INTEGER PRIMARY KEY AUTOINCREMENT,
-//     nome TEXT NOT NULL,
-//     email TEXT NOT NULL,
-//     macid TEXT NOT NULL,
-//     data_cadastro TEXT DEFAULT (datetime('now')),
-//     cpf TEXT,
-//     status TEXT,
-//     password TEXT
-//   )`);
+//Criação das tabelas
+db.serialize(() => {
+  // Tabela de usuários
+  db.run(`CREATE TABLE IF NOT EXISTS usuarios (
+    uid INTEGER PRIMARY KEY AUTOINCREMENT,
+    nome TEXT NOT NULL,
+    email TEXT NOT NULL,
+    macid TEXT NOT NULL,
+    data_cadastro TEXT DEFAULT (datetime('now')),
+    cpf TEXT,
+    status TEXT,
+    password TEXT
+  )`);
 
-//   // Tabela de planos
-//   db.run(`CREATE TABLE IF NOT EXISTS planos (
-//     id INTEGER PRIMARY KEY AUTOINCREMENT,
-//     nome_plano TEXT,
-//     tipo_plano TEXT,   -- 'mensal' ou 'anual'
-//     preco REAL,
-//     status TEXT
-//   )`);
+  // Tabela de planos
+  db.run(`CREATE TABLE IF NOT EXISTS planos (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    nome_plano TEXT,
+    tipo_plano TEXT,   -- 'mensal' ou 'anual'
+    preco REAL,
+    status TEXT
+  )`);
 
-//   // Tabela de assinantes
-//   db.run(`CREATE TABLE IF NOT EXISTS assinantes (
-//     id INTEGER PRIMARY KEY AUTOINCREMENT,
-//     userid INTEGER,
-//     plano_id INTEGER,
-//     data_compra TEXT,
-//     data_start TEXT,
-//     data_end TEXT,
-//     status TEXT,
-//     FOREIGN KEY(userid) REFERENCES usuarios(uid),
-//     FOREIGN KEY(plano_id) REFERENCES planos(id)
-//   )`);
-// });
+  // Tabela de assinantes
+  db.run(`CREATE TABLE IF NOT EXISTS assinantes (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    userid INTEGER,
+    plano_id INTEGER,
+    data_compra TEXT,
+    data_start TEXT,
+    data_end TEXT,
+    status TEXT,
+    FOREIGN KEY(userid) REFERENCES usuarios(uid),
+    FOREIGN KEY(plano_id) REFERENCES planos(id)
+  )`);
+});
 
 module.exports = db;
